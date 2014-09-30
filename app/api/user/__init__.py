3f16d4d86bfe6e77b88ca6062719ff52241940bf
Vivek R <vividvilla@gmail.com>
2014-09-30 22:51:54 +0530

    Fixed user base auth

--- app/api/user/__init__.py
+++ app/api/user/__init__.py
@@ -1,26 +1,26 @@
 from app import app
 from app import db
-from app.model import User
+from app.model.user import User
 from app import utils
 
-class User(object):
+class Api(object):
 
     def __init__(self, **details):
         self.username = details.get("username", None)
-        self.email = details.get("username", None)
+        self.email = details.get("email", None)
         self.real_name = details.get("real_name", '')
         self.website = details.get("website", '')
         self.bio = details.get("bio", '')
 
     def create(self, **details):
         if not (self.username and self.email):
-            raise
+            raise ValueError("Either username or email is empty")
 
-        if username_exists(self.username):
-            raise
+        if self.username_exists(self.username):
+            raise ValueError("Username already exists")
 
-        if email_exists(self.email):
-            raise
+        if self.email_exists(self.email):
+            raise ValueError("Email already exists")
 
         new_user = User()
         new_user.username = self.username
@@ -31,24 +31,29 @@ class User(object):
         db.session.add(new_user)
         db.session.commit()
 
-        return get_user_id(username = self.username)
+        return self.get_id(username = self.username)
 
     def delete(self, username):
-        pass
+        user = self.get(username = username)
+        if user:
+            db.session.delete(user)
+            db.session.commit()
+        else:
+            raise ValueError("User does not exist")
 
     def get(self, username = False, email = False):
         if username:
-            return db.session.query().filter_by(username = username).first()
+            return db.session.query(User).filter_by(username = username).first()
         elif email:
-            return db.session.query().filter_by(username = username).first()
+            return db.session.query(User).filter_by(email = email).first()
         else:
             return None
 
-    def get_id(self, **details):
+    def get_id(self, username = False, email = False):
         if username:
-            return get(username = username).id
+            return self.get(username = username).id
         elif email:
-            return get(email = email).id
+            return self.get(email = email).id
         else:
             return None
 
@@ -56,7 +61,7 @@ class User(object):
         pass
 
     def username_exists(self, username):
-        return get(username = username)
+        return self.get(username = username)
 
     def email_exists(self, email):
-        return get(email = email)
+        return self.get(email = email)
--- app/model/user.py
+++ app/model/user.py
@@ -1,6 +1,7 @@
 from app import db
 
 class User(db.Model):
+    __tablename__ = 'users'
     id = db.Column(db.Integer, primary_key = True)
     username = db.Column(db.String(20), unique = True)
     email = db.Column(db.String(100), unique = True)
