2401c4dfec5ee942524241618b220fe2e3c9d135
Vivek R <vividvilla@gmail.com>
2014-10-12 00:48:48 +0530

    Added commandline option for running tests

--- README.md
+++ README.md
@@ -22,3 +22,6 @@ To Do's
 - [ ] Sperate table for roles
 - [ ] Set nullable = flase for required fields
 - [ ] Set default for fields in model declaration itself
+- [ ] Rewrite user model
+- [ ] Rewrite tests for user model
+- [ ] Write tests for question and tag model
--- manage.py
+++ manage.py
@@ -1,13 +1,32 @@
 from flaq import app, db
-from flask.ext.script import Manager
+from flask.ext.script import Manager, Shell
 from flask.ext.migrate import Migrate, MigrateCommand
 
 #Import models to get discovered by our app
 import flaq.models
 
+#Adding command shell for database migrations
 migrate = Migrate(app, db)
 manager = Manager(app)
+
+#Initialize shell with pre imports for testing
+def make_shell_context():
+    return dict(
+        app = app,
+        db = db,
+        user = flaq.models.user,
+        question = flaq.models.question)
+
+#Add shell command alias
 manager.add_command('db', MigrateCommand)
+manager.add_command('shell', Shell(make_context=make_shell_context))
+
+@manager.command
+def test():
+    """Run unit tests(under tests folder) through commandline"""
+    import unittest
+    tests = unittest.TestLoader().discover('tests')
+    unittest.TextTestRunner(verbosity=2).run(tests)
 
 if __name__ == '__main__':
     manager.run()
